927dbad98dfa013c183a1bc62fe10cf36e7f47de
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-08-07 17:53:00 -0400

    Added X and Y axis.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
+let xAxis = canvas.width / 2;
+let yAxis = canvas.height - 30;
+
 const draw = () => {
   ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.arc(xAxis, yAxis, 10, 0, Math.PI * 2);
   ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
